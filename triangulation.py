a75404a6a13eecf74c6eea73a04aa6b876494382
weatherdude <97835234+weatherdude@users.noreply.github.com>
2023-07-23 18:16:16 +0200

    changed uniform error dist to normal dist

--- triangulation.py
+++ triangulation.py
@@ -1,14 +1,17 @@
 import math
+import random
 import numpy as np 
 
 ## calculate distance to object from azimuth and elevation angles
-alpha1 = 45     # fisheye azimuth (°)
-beta1 = 60      # fisheye elevation (°)
-alpha2 = 45     # PTF azimuth (°)
-beta2 = 60      # PTF elevation (°)
-c = 0.4         # distance between both cameras (m)
-A = [0,0,0]     # fisheye coordinates
-B = [c,0,0]     # PTF coordinates
+alpha1 = 45         # fisheye azimuth (°)
+beta1 = 60          # fisheye elevation (°)
+alpha2 = 45         # PTF (or 2nd fisheye) azimuth (°)
+beta2 = 60          # PTF (or 2nd fisheye) elevation (°)
+c = 0.4             # distance between both cameras (m)
+A = [0,0,0]         # fisheye coordinates
+B = [c,0,0]         # PTF coordinates
+alpha1_unc = 0.1    # error in alpha1 (°)
+beta1_unc = 0.1     # error in beta1 (°)
 
 alpha3 = 180 - alpha1 - alpha2
 b = math.sin(alpha2*math.pi/180) * c/math.sin(alpha3*math.pi/180)   # horizontal distance Fisheye - Object
@@ -48,12 +51,20 @@ alpha2 = math.acos((c-dx)/a) * 180/math.pi
 # print("alpha1 ="+ str(alpha1))
 # print("alpha2 ="+ str(alpha2))
 
-# now calculate uncertainty in distance calc based on assumed error of 0.1° in alpha1 and beta1
+# now calculate uncertainty in distance calc based on assumed error in alpha1 and beta1
+# we assume that the error is normally distributed with a chance of 3 standard dev. that the given error is exceeded
+mean = 0
+stdev = alpha1_unc/3
+alpha1_unc_list = [random.gauss(mean, stdev) for _ in range(1000)]
+
+stdev = beta1_unc/3
+beta1_unc_list = [random.gauss(mean, stdev) for _ in range(1000)]
+
 dist_list = []
-for i in np.linspace(-0.1,0.1,1000):
-    alpha_unc = alpha1+i
-    for j in np.linspace(-0.1,0.1,1000):
-        beta_unc=beta1+j
+for i in range(0,len(alpha1_unc_list)):
+    alpha_unc = alpha1+alpha1_unc_list[i]
+    for j in range(0,len(beta1_unc_list)):
+        beta_unc=beta1+beta1_unc_list[j]
         alpha3 = 180 - alpha_unc - alpha2
         b = math.sin(alpha2*math.pi/180) * c/math.sin(alpha3*math.pi/180)   # horizontal distance Fisheye - Object
         b1 = b/math.cos(beta_unc*math.pi/180)                               # distance Fisheye - object
--- triangulation_slider.py
+++ triangulation_slider.py
@@ -3,19 +3,30 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.widgets import Slider
 import math
+import random
 import numpy as np
+import matplotlib as mpl
 
 c = 0.4 # distance between both cameras (m)
 A = [0,0,0]   # fisheye coordinates
-B = [c,0,0]   # PTF coordinates
-object_coordinates = [1,50,100] # x,y,z coordinates (m)
+B = [c,0,0]   # PTF (or 2nd fisheye) coordinates
+object_coordinates = [1,50,1000] # x,y,z coordinates (m)
+alpha1_unc = 0.02   # error in alpha1 (°)
+beta1_unc = 0.02    # error in beta1 (°)
+
 # Initial coordinates of the triangle
-x0, y0, z0 = 0.0, 0.0, 0.0 # fisheye
-x1, y1, z1 = c, 0, 0 # PTF
+x0, y0, z0 = 0.0, 0.0, 0.0  # fisheyes
+x1, y1, z1 = c, 0, 0        # PTF (or 2nd fisheye)
 x2, y2, z2 = object_coordinates[0], object_coordinates[1], object_coordinates[2]
 
+# Get the monitor screen size
+default_dpi = mpl.rcParams['figure.dpi']
+monitor_size = (1920, 1080)  # Replace this with your monitor's resolution if necessary
+fig_width = monitor_size[0] * 0.75 / default_dpi
+fig_height = monitor_size[1] * 0.75 / default_dpi
+print(fig_width)
 # Create the 3D plot
-fig = plt.figure()
+fig = plt.figure(figsize=(fig_width, fig_height))
 ax = fig.add_subplot(221, projection='3d')
 ax2 = fig.add_subplot(222)
 # Function to update the plot based on the slider values
@@ -23,6 +34,8 @@ def update(val):
     global x0, y0, z0, x1, y1, z1, x2, y2, z2
     x1 = slider_dist.val
     x2, y2, z2 = slider_x.val, slider_y.val, slider_z.val
+    alpha1_unc = slider_alpha_unc.val
+    beta1_unc = slider_beta_unc.val
 
     # Clear previous plot and create the updated triangle
     ax.clear()
@@ -55,12 +68,20 @@ def update(val):
     alpha1 = math.acos(dx/b) * 180/math.pi
     alpha2 = math.acos((c-dx)/a) * 180/math.pi
 
-    # now calculate uncertainty in distance calc based on assumed error of 0.1° in the determined angles
+    # now calculate uncertainty in distance calc based on assumed error in the determined angles
+    # we assume that the error is normally distributed with a chance of 3 standard dev. that the given error is exceeded
+    mean = 0
+    stdev = alpha1_unc/3
+    alpha1_unc_list = [random.gauss(mean, stdev) for _ in range(100)]
+
+    stdev = beta1_unc/3
+    beta1_unc_list = [random.gauss(mean, stdev) for _ in range(100)]
+
     dist_list = []
-    for i in np.linspace(-0.1,0.1,100):
-        alpha_unc = alpha1+i
-        for j in np.linspace(-0.1,0.1,100):
-            beta_unc=beta1+j
+    for i in range(0,len(alpha1_unc_list)):
+        alpha_unc = alpha1+alpha1_unc_list[i]
+        for j in range(0,len(beta1_unc_list)):
+            beta_unc=beta1+beta1_unc_list[j]
             alpha3 = 180 - alpha_unc - alpha2
             b = math.sin(alpha2*math.pi/180) * c/math.sin(alpha3*math.pi/180) # horizontal distance Fisheye - Object
             b1 = b/math.cos(beta_unc*math.pi/180) # distance Fisheye - object
@@ -68,9 +89,16 @@ def update(val):
     
     ax2.clear()
     ax2.hist(dist_list, density=False, bins=100)  # density=False would make counts
+    ax2.set_xlabel('Distance uncertainty (m)')
+    ax2.set_ylabel('Count')
     # ax2.ylabel('Counts')
     # ax2.xlabel('Distances')
-
+    # Add the annotation box
+    ax.text2D(0.00, -0.40, "Distance Fisheye-Object = "+str(round(b1,2))+" m", transform=ax.transAxes, fontsize=12,
+              bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+    ax.text2D(2.25, -0.40, "Uncertainty range = "+str(round(max(dist_list)-min(dist_list),2))+" m", transform=ax.transAxes, fontsize=12,
+              bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+    
     fig.canvas.draw_idle()
 
 # Create sliders for each coordinate
@@ -78,16 +106,22 @@ ax_dist = plt.axes([0.25, 0.02, 0.45, 0.03], facecolor='lightgoldenrodyellow')
 ax_x = plt.axes([0.25, 0.07, 0.45, 0.03], facecolor='lightgoldenrodyellow')
 ax_y = plt.axes([0.25, 0.12, 0.45, 0.03], facecolor='lightgoldenrodyellow')
 ax_z = plt.axes([0.25, 0.17, 0.45, 0.03], facecolor='lightgoldenrodyellow')
+ax_alpha_unc = plt.axes([0.25, 0.22, 0.45, 0.03], facecolor='lightgoldenrodyellow')
+ax_beta_unc = plt.axes([0.25, 0.27, 0.45, 0.03], facecolor='lightgoldenrodyellow')
 
-slider_dist = Slider(ax_dist, 'Distance cameras', 0.1, 100.0, valinit=c)
+slider_dist = Slider(ax_dist, 'Distance cameras (m)', 0.1, 100.0, valinit=c)
 slider_x = Slider(ax_x, 'Object x pos. (m)', -100.0, 100.0, valinit=object_coordinates[0])
 slider_y = Slider(ax_y, 'Object y pos. (m)', -100.0, 100.0, valinit=object_coordinates[1])
-slider_z = Slider(ax_z, 'Object height (m)', 0, 100.0, valinit=object_coordinates[2])
+slider_z = Slider(ax_z, 'Object height (m)', 0, 10000.0, valinit=object_coordinates[2])
+slider_alpha_unc = Slider(ax_alpha_unc, 'Azimuth uncertainty (°)', 0.01, 1, valinit=alpha1_unc)
+slider_beta_unc = Slider(ax_beta_unc, 'Elevation uncertainty (°)', 0.01, 1, valinit=beta1_unc)
 
 # Call the update function when the slider values change
 slider_dist.on_changed(update)
 slider_x.on_changed(update)
 slider_y.on_changed(update)
 slider_z.on_changed(update)
+slider_alpha_unc.on_changed(update)
+slider_beta_unc.on_changed(update)
 
 plt.show()
